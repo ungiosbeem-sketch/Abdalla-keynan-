a0a911426583e548d25978848f4e3dc872be0a0e
ungiosbeem-sketch <ungiosbeem@gmail.com>
2026-06-07 23:38:27 +0300

    Create config.ts

---
+++ src/services/firebase/config.ts
@@ -0,0 +1,21 @@
+import { initializeApp, getApps, getApp } from 'firebase/app';
+import Constants from 'expo-constants';
+
+const firebaseConfig = {
+  apiKey: Constants.expoConfig?.extra?.firebaseApiKey || process.env.FIREBASE_API_KEY,
+  authDomain: Constants.expoConfig?.extra?.firebaseAuthDomain || process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: Constants.expoConfig?.extra?.firebaseProjectId || process.env.FIREBASE_PROJECT_ID,
+  storageBucket: Constants.expoConfig?.extra?.firebaseStorageBucket || process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: Constants.expoConfig?.extra?.firebaseMessagingSenderId || process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: Constants.expoConfig?.extra?.firebaseAppId || process.env.FIREBASE_APP_ID,
+  measurementId: Constants.expoConfig?.extra?.firebaseMeasurementId || process.env.FIREBASE_MEASUREMENT_ID,
+};
+
+export const initializeFirebase = () => {
+  if (!getApps().length) {
+    initializeApp(firebaseConfig);
+  }
+  return getApp();
+};
+
+export const app = initializeFirebase();
